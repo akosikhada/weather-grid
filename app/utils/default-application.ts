d51a07410621b0efe158113e193c9b3a28c4ee66
akosikhada <miguel.enrique.dasalla.github@gmail.com>
2025-04-21 09:52:08 +0800

    feat(utils): add default application constants for location and population
    - Defined default latitude and longitude for Caloocan City, Philippines.
    - Included default city name and population based on the 2020 census.
    - Provided central storage for reusable application constants.

---
+++ app/utils/default-application.ts
@@ -0,0 +1,11 @@
+/**
+ * Application constants
+ *
+ * This file contains constants used throughout the application
+ */
+
+// Default location coordinates for Caloocan City, Philippines
+export const DEFAULT_LATITUDE = 14.65;
+export const DEFAULT_LONGITUDE = 120.9667;
+export const DEFAULT_CITY_NAME = "Caloocan City";
+export const DEFAULT_CITY_POPULATION = 1661584; // Based on 2020 census
