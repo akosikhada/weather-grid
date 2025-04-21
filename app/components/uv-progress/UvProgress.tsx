3ae8dcd9c59b8287f17fe5199eab597f05bc94a0
akosikhada <miguel.enrique.dasalla.github@gmail.com>
2025-04-21 09:50:48 +0800

    feat(uv-progress): add UV Progress component for visualizing UV index
    - Implemented reusable progress bar component using `@radix-ui/react-progress`.
    - Integrated forwardRef for better flexibility and React compatibility.
    - Styled component with responsive design and dark mode support for seamless integration.

---
+++ app/components/uv-progress/UvProgress.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import * as React from "react";
+import * as ProgressPrimitive from "@radix-ui/react-progress";
+
+import { cn } from "@/lib/utils";
+
+const UvProgress = React.forwardRef<
+  React.ElementRef<typeof ProgressPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
+>(({ className, value, ...props }, ref) => (
+  <ProgressPrimitive.Root
+    ref={ref}
+    className={cn(
+      "bg-secondary relative h-3 w-full overflow-hidden rounded-full",
+      className,
+    )}
+    {...props}
+  >
+    <ProgressPrimitive.Indicator
+      className="bg-primary h-3 w-3 flex-1 rounded-full shadow-lg ring-2 shadow-white dark:ring-gray-500"
+      style={{ marginLeft: `${value}%` }}
+    />
+  </ProgressPrimitive.Root>
+));
+UvProgress.displayName = ProgressPrimitive.Root.displayName;
+
+export { UvProgress };
