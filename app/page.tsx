583cbf25c99f7317947d14c89d22095307db442d
akosikhada <miguel.enrique.dasalla.github@gmail.com>
2025-04-21 09:51:51 +0800

    feat(home-page): integrate UV Index and Population components
    - Imported `UvIndex` and `Population` components for enhanced weather visualization.
    - Added `UvIndex` and `Population` to the grid layout for seamless integration with existing components.
    - Ensured responsive design and cohesive layout styling.

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,13 @@ import Sunset from "./components/sunset/Sunset";
 import Temperature from "./components/temperature/Temperature";
 import Wind from "./components/wind-compass/Wind";
 import DailyForecast from "./components/daily-forecast/DailyForecast";
-
+import UvIndex from "./components/uv-index/UvIndex";
+import Population from "./components/population/Population";
 export default function Home() {
   return (
     <main className="m-auto mx-[1rem] lg:mx-[2rem] xl:mx-[6rem] 2xl:mx-[16rem]">
       <NavBar />
-      <div className="flex flex-col gap-4 pb-4 md:flex-row">
+      <div className="flex flex-col gap-4 py-4 md:flex-row">
         <div className="flex w-full min-w-[18rem] flex-col gap-4 md:w-[35rem]">
           <Temperature />
         </div>
@@ -19,6 +20,8 @@ export default function Home() {
             <Sunset />
             <Wind />
             <DailyForecast />
+            <UvIndex />
+            <Population />
           </div>
         </div>
       </div>
