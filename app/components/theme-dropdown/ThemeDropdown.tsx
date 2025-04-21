0ea52ae211d0daba76418323bc286ca5398730ef
akosikhada <miguel.enrique.dasalla.github@gmail.com>
2025-04-21 09:50:04 +0800

    feat(theme-dropdown): enhance UI with system default theme option
    - Added a new `System Default` option with the monitor icon for theme preferences.
    - Improved button layout and styling for better responsiveness across screen sizes.
    - Updated dropdown menu content alignment and interactive elements for clarity and usability.

--- app/components/theme-dropdown/ThemeDropdown.tsx
+++ app/components/theme-dropdown/ThemeDropdown.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as React from "react";
-import { Moon, Sun } from "lucide-react";
+import { Monitor, Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
 import { Button } from "@/components/ui/button";
@@ -18,18 +18,33 @@ export function ThemeDropdown() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-          <span className="sr-only">Toggle theme</span>
+        <Button variant="outline" className="h-9 cursor-pointer px-3 sm:h-9">
+          <div className="relative flex items-center">
+            <Sun className="h-4 w-4 scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+            <Moon className="absolute h-4 w-4 scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+            <Monitor className="hidden h-4 w-4 scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+          </div>
+          <span className="text-sm">Theme Preferences</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
+      <DropdownMenuContent align="center">
+        <DropdownMenuItem
+          onClick={() => setTheme("light")}
+          className="cursor-pointer"
+        >
+          <Sun size={15} /> Light Mode
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
+        <DropdownMenuItem
+          onClick={() => setTheme("dark")}
+          className="cursor-pointer"
+        >
+          <Moon size={15} /> Dark Mode
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          onClick={() => setTheme("system")}
+          className="cursor-pointer"
+        >
+          <Monitor size={15} /> System Default
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
